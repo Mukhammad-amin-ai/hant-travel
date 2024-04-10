154d896ab65703e3f4ac96f172410df0aa160d03
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-04-10 15:21:50 +0500

    i dont no

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -3,10 +3,6 @@ import React from 'react';
 const Modal = () => {
   return (
     <>
-      {/*<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">*/}
-      {/*  Launch demo modal*/}
-      {/*</button>*/}
-
       <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel"
            aria-hidden="true">
         <div className="modal-dialog">
@@ -16,12 +12,12 @@ const Modal = () => {
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-              ...
+              Your Message Have Sent
             </div>
-            {/*<div className="modal-footer">*/}
-            {/*  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>*/}
-            {/*  <button type="button" className="btn btn-primary">Save changes</button>*/}
-            {/*</div>*/}
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Ok</button>
+              {/*<button type="button" className="btn btn-primary">Save changes</button>*/}
+            </div>
           </div>
         </div>
       </div>
