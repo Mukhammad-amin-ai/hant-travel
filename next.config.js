4153499ebc5974ac3b87df48f4227dbc3e0bfae2
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-04-11 22:48:35 +0500

    mionifier

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+const { default: classnamesMinifier } = require('@nimpl/classnames-minifier');
 module.exports = nextConfig
+
+module.exports = (phase) => classnamesMinifier({
+  prefix: '_',
+  reservedNames: ['_en', '_de'],
+})(nextConfig);
