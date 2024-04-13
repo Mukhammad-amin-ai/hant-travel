822ca03d3744c6462415e76a133653869243c4c0
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-04-13 15:21:33 +0500

    partially compleated pagination

--- src/app/package/page.js
+++ src/app/package/page.js
@@ -9,13 +9,13 @@ import Tour from "../../data/custom/tour.json";
 import destination from "../../data/custom/destination.json";
 import Type from "../../data/custom/type.json";
 import {useSearchParams} from "next/navigation";
-import { useRouter } from 'next/navigation';
+import {useRouter} from 'next/navigation';
 
 const page = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const search = searchParams.get("search");
-  const typeOdtour = searchParams.get("type");
+  const typeOdour = searchParams.get("type");
   const days = searchParams.get("day");
   const [checkedIndex, setCheckedIndex] = useState(-1);
   const [tourCheck, setTourCheck] = useState(-1);
@@ -24,14 +24,15 @@ const page = () => {
   const [tour, setTour] = useState(Tour);
   const [countryCheck, setCountryCheck] = useState(false)
   const [typeCheck, setTypeCheck] = useState(false)
-
   const fromDay = useRef(null);
   const selectPrice = useRef(null);
+
   let handleCheckboxChange = (index, name) => {
     setCheckedIndex(index);
     setNameTour(name);
     setCountryCheck(true)
   };
+
   const tourCheckFunc = (index, type) => {
     setTourCheck(index);
     setTourType(type);
@@ -83,38 +84,70 @@ const page = () => {
   let countryIdFinder = () => {
     let country = destination;
     if (search !== null) {
-      let idftour = country.filter((item) => item.name === search);
-      if (idftour.length === 0) {
+      let idOfTour = country.filter((item) => item.name === search);
+      if (idOfTour.length === 0) {
         router.push('/not_found')
       } else {
-        return idftour[0]?.id;
+        return idOfTour[0]?.id;
       }
     }
   };
 
   let typeFinder = () => {
     let typeTour = Type;
-    if (typeOdtour !== null) {
-      let idftour = typeTour.filter((item) => item.type === typeOdtour);
-      // console.log(idftour.length === 0)
-      if (idftour.length === 0) {
+    if (typeOdour !== null) {
+      let idOfTour = typeTour.filter((item) => item.type === typeOdour);
+      if (idOfTour.length === 0) {
         router.push('/not_found')
       } else {
-        return idftour[0]?.id;
+        return idOfTour[0]?.id;
       }
     }
   };
 
+  const [currentPage, setCurrentPage] = useState(1)
+  let itemsPerPage = 6
+  const totalPages = Math.ceil(Tour.length / itemsPerPage)
+
+  const pageNumbers = Array.from({length: totalPages}, (_, index) => index + 1);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const sliceFunc = () => {
+    return setTour(Tour.slice(startIndex, endIndex))
+  }
+
+
+  const handleNextPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+      console.log(currentPage)
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+      console.log(currentPage)
+    }
+
+  };
+
+
   useEffect(() => {
-    setTour(Tour.slice(0, 6))
+    sliceFunc()
     let countryId = countryIdFinder() - 1;
-    let tourType =  typeFinder() - 1;
+    let tourType = typeFinder() - 1;
 
     if (search !== null) {
       handleCheckboxChange(countryId, search);
     }
-    if (typeOdtour !== null) {
-      tourCheckFunc(tourType, typeOdtour);
+    if (typeOdour !== null) {
+      tourCheckFunc(tourType, typeOdour);
     }
     if (days !== null) {
       let dayInput = document.getElementById("from_input");
@@ -125,7 +158,7 @@ const page = () => {
     }
 
 
-  }, []);
+  }, [currentPage, Tour]);
 
   return (
     <>
@@ -140,10 +173,6 @@ const page = () => {
                 </p>
                 <div className="selector-and-grid">
                   <div className="selector">
-                    {/* <SelectComponent
-                                            options={["Price Low to Hig", "Price High to Low"]}
-                                           placeholder="Default Sorting"
-                                        /> */}
                     <select className="form-select"
                             aria-label="Default select example"
                             ref={selectPrice}
@@ -195,7 +224,13 @@ const page = () => {
                           <div className="package-card-content">
                             <div className="card-content-top">
                               <h5>
-                                <Link href={`/package/${item.id}`}  style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden",width:"300px",display:"block"}}>
+                                <Link href={`/package/${item.id}`} style={{
+                                  whiteSpace: "nowrap",
+                                  textOverflow: "ellipsis",
+                                  overflow: "hidden",
+                                  width: "300px",
+                                  display: "block"
+                                }}>
                                   {item.title}
                                 </Link>
                               </h5>
@@ -239,121 +274,29 @@ const page = () => {
                       </div>
                     )) : "NODATA"
                   }
-
-                  {/*// {tour.map((item, index) => (*/}
-                  {/*//   <div className="col-md-6 item" key={index}>*/}
-                  {/*//     <div className="package-card">*/}
-                  {/*//       <div className="package-card-img-wrap">*/}
-                  {/*//         <Link href={`/package/${item.id}`}*/}
-                  {/*//               className="card-img">*/}
-                  {/*//           <img*/}
-                  {/*//             style={{height: "177px", width: "100%"}}*/}
-                  {/*//             src={item.img}*/}
-                  {/*//             alt=""/>*/}
-                  {/*//         </Link>*/}
-                  {/*//         <div className="batch">*/}
-                  {/*//           <span className="date">{item.batch}</span>*/}
-                  {/*//           <div className="location">*/}
-                  {/*//             <svg*/}
-                  {/*//               xmlns="http://www.w3.org/2000/svg"*/}
-                  {/*//               width={18}*/}
-                  {/*//               height={18}*/}
-                  {/*//               viewBox="0 0 18 18"*/}
-                  {/*//             >*/}
-                  {/*//               <path*/}
-                  {/*//                 d="M8.99939 0C5.40484 0 2.48047 2.92437 2.48047 6.51888C2.48047 10.9798 8.31426 17.5287 8.56264 17.8053C8.79594 18.0651 9.20326 18.0646 9.43613 17.8053C9.68451 17.5287 15.5183 10.9798 15.5183 6.51888C15.5182 2.92437 12.5939 0 8.99939 0ZM8.99939 9.79871C7.19088 9.79871 5.71959 8.32739 5.71959 6.51888C5.71959 4.71037 7.19091 3.23909 8.99939 3.23909C10.8079 3.23909 12.2791 4.71041 12.2791 6.51892C12.2791 8.32743 10.8079 9.79871 8.99939 9.79871Z"/>*/}
-                  {/*//             </svg>*/}
-                  {/*//             <ul className="location-list">*/}
-                  {/*//               {item.location_list.map((item, index) => (*/}
-                  {/*//                 <li key={index}>*/}
-                  {/*//                   <Link href="/package">{item}</Link>*/}
-                  {/*//                 </li>*/}
-                  {/*//               ))}*/}
-                  {/*//             </ul>*/}
-                  {/*//           </div>*/}
-                  {/*//         </div>*/}
-                  {/*//       </div>*/}
-                  {/*//       <div className="package-card-content">*/}
-                  {/*//         <div className="card-content-top">*/}
-                  {/*//           <h5>*/}
-                  {/*//             <Link href={`/package/${item.id}`}  style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden",width:"300px",display:"block"}}>*/}
-                  {/*//               {item.title}*/}
-                  {/*//             </Link>*/}
-                  {/*//           </h5>*/}
-                  {/*//           <div className="location-area">*/}
-                  {/*//             <ul className="location-list scrollTextAni">*/}
-                  {/*//               {item.place_list.map((item, index) => (*/}
-                  {/*//                 <li key={index}>*/}
-                  {/*//                   <Link href="/package">{item}</Link>*/}
-                  {/*//                 </li>*/}
-                  {/*//               ))}*/}
-                  {/*//             </ul>*/}
-                  {/*//           </div>*/}
-                  {/*//         </div>*/}
-                  {/*//         <div className="card-content-bottom">*/}
-                  {/*//           <div className="price-area">*/}
-                  {/*//             <h6>Starting Form:</h6>*/}
-                  {/*//             <span>$ {item.price}</span>*/}
-                  {/*//             <p>TAXES INCL/PERS</p>*/}
-                  {/*//           </div>*/}
-                  {/*//           <Link*/}
-                  {/*//             href={`/package/${item.id}`}*/}
-                  {/*//             className="primary-btn2"*/}
-                  {/*//           >*/}
-                  {/*//             Book a Trip*/}
-                  {/*//             <svg*/}
-                  {/*//               xmlns="http://www.w3.org/2000/svg"*/}
-                  {/*//               width={18}*/}
-                  {/*//               height={18}*/}
-                  {/*//               viewBox="0 0 18 18"*/}
-                  {/*//               fill="none"*/}
-                  {/*//             >*/}
-                  {/*//               <path*/}
-                  {/*//                 d="M8.15624 10.2261L7.70276 12.3534L5.60722 18L6.85097 17.7928L12.6612 10.1948C13.4812 10.1662 14.2764 10.1222 14.9674 10.054C18.1643 9.73783 17.9985 8.99997 17.9985 8.99997C17.9985 8.99997 18.1643 8.26211 14.9674 7.94594C14.2764 7.87745 13.4811 7.8335 12.6611 7.80518L6.851 0.206972L5.60722 -5.41705e-07L7.70276 5.64663L8.15624 7.77386C7.0917 7.78979 6.37132 7.81403 6.37132 7.81403C6.37132 7.81403 4.90278 7.84793 2.63059 8.35988L0.778036 5.79016L0.000253424 5.79016L0.554115 8.91458C0.454429 8.94514 0.454429 9.05483 0.554115 9.08539L0.000253144 12.2098L0.778036 12.2098L2.63059 9.64035C4.90278 10.1523 6.37132 10.1857 6.37132 10.1857C6.37132 10.1857 7.0917 10.2102 8.15624 10.2261Z"/>*/}
-                  {/*//               <path*/}
-                  {/*//                 d="M12.0703 11.9318L12.0703 12.7706L8.97041 12.7706L8.97041 11.9318L12.0703 11.9318ZM12.0703 5.23292L12.0703 6.0714L8.97059 6.0714L8.97059 5.23292L12.0703 5.23292ZM9.97892 14.7465L9.97892 15.585L7.11389 15.585L7.11389 14.7465L9.97892 14.7465ZM9.97892 2.41846L9.97892 3.2572L7.11389 3.2572L7.11389 2.41846L9.97892 2.41846Z"/>*/}
-                  {/*//             </svg>*/}
-                  {/*//           </Link>*/}
-                  {/*//         </div>*/}
-                  {/*//       </div>*/}
-                  {/*//     </div>*/}
-                  {/*//   </div>*/}
-                  {/*// ))}*/}
                 </div>
               </div>
-              {tour.length > 6 ? (
+              {Tour.length > 6 ? (
                 <div className="row">
                   <div className="col-lg-12">
                     <nav className="inner-pagination-area">
                       <ul className="pagination-list">
                         <li>
-                          <a href="#" className="shop-pagi-btn">
+                          <div onClick={() => handlePrevPage()} className="shop-pagi-btn">
                             <i className="bi bi-chevron-left"/>
-                          </a>
+                          </div>
                         </li>
+                        {
+                          pageNumbers.map((pageNumber, index) => (
+                            <li key={index} type="button" onClick={() => handlePageChange(pageNumber)}>
+                              {pageNumber}
+                            </li>
+                          ))
+                        }
                         <li>
-                          <a href="#">1</a>
-                        </li>
-                        <li>
-                          <a href="#" className="active">
-                            2
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">3</a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="bi bi-three-dots"/>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">6</a>
-                        </li>
-                        <li>
-                          <a href="#" className="shop-pagi-btn">
+                          <div onClick={() => handleNextPage()} className="shop-pagi-btn">
                             <i className="bi bi-chevron-right"/>
-                          </a>
+                          </div>
                         </li>
                       </ul>
                     </nav>
@@ -411,7 +354,7 @@ const page = () => {
                     <div className="accordion-item">
                       <h2 className="accordion-header">
                         <button
-                          className={`accordion-button  ${typeOdtour !== null ? "" : "collapsed"} collapsed`}
+                          className={`accordion-button  ${typeOdour !== null ? "" : "collapsed"} collapsed`}
                           type="button"
                           data-bs-toggle="collapse"
                           data-bs-target="#collapseTwo" aria-expanded="true"
@@ -420,7 +363,7 @@ const page = () => {
                         </button>
                       </h2>
                       <div id="collapseTwo"
-                           className={`accordion-collapse  ${typeOdtour !== null ? "show" : "collapse"}`}
+                           className={`accordion-collapse  ${typeOdour !== null ? "show" : "collapse"}`}
                            data-bs-parent="#accordionExample">
                         <div className="accordion-body">
                           <div className="row g-3">
