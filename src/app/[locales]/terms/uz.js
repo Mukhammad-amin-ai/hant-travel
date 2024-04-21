76ae42d1719bf5e26e1af8012fa8973c05f3dec2
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-04-21 20:36:52 +0500

    added language

---
+++ src/app/[locales]/terms/uz.js
@@ -0,0 +1,230 @@
+import React from 'react';
+
+const uz = () => {
+  return (
+    <>
+      < h1>qoida va shartlar</h1>
+      < p>oxirgi yangilangan: aprel 13, 2024< / p>
+      < p>iltimos, xizmatimizdan foydalanishdan oldin ushbu shartlarni diqqat bilan o'qib chiqing.< / p>
+      < h2>interpretatsiya va ta'riflar</h2>
+      < h3>talqin< / h3>
+      <p> bosh harfi bosh harf bilan yozilgan so'zlar quyidagi shartlarda belgilangan ma'nolarga ega. The
+        quyidagi ta'riflar birlikda yoki ichida paydo bo'lishidan qat'i nazar, bir xil ma'noga ega bo'lishi kerak
+        ko'plik.< / p>
+      < h3>ta'riflar< / h3>
+      < p>ushbu shartlar uchun:< / p>
+      < ul>
+        <li>
+          <p>< strong>Affiliate < / strong> boshqaruvchi, boshqariladigan yoki umumiy nazorat ostida bo'lgan shaxsni
+            anglatadi
+            bir partiya ,bo'ldi & quote; nazorat&quote; egalik anglatadi 50% aktsiyalar yoki undan ko'p, xolislik foiz
+            yoki boshqa
+            qimmatli qog'ozlar direktorlar yoki boshqa boshqaruv organi saylash uchun ovoz berish huquqiga ega.< / p>
+        < / li>
+        <li>
+          <p>< strong>mamlakat < / strong> deganda: O'zbekiston< / p>
+        < / li>
+        <li>
+          <p>< strong>kompaniya< / strong>(yo &quot;deb ataladi
+            Kompaniya &quote;,&quote;biz &quote;,&quote;bizga &quote; yoki&quote;bizning & quote; ushbu shartnomada)
+            Hant sayohat anglatadi,
+            O'zbekiston Samarqand .< / p>
+        < / li>
+        <li>
+          <p><strong>qurilma < /strong> xizmatga kira oladigan har qanday qurilma, masalan, kompyuter, uyali telefon
+            yoki
+            raqamli planshet.< / p>
+        < / li>
+        <li>
+          <p><strong>xizmati < /strong> veb-saytga ishora qiladi.< / p>
+        < / li>
+        <li>
+          <p>< strong>shartlar va shartlar < / strong> (&quot;shartlar&quot; deb ham ataladi) bu shartlar va shartlarni
+            anglatadi
+            bu siz va kompaniya o'rtasida xizmatdan foydalanish bo'yicha to'liq kelishuvni tashkil qiladi. Bu shartlar
+            va
+            Shartlar shartnomasi <a yordamida yaratilgan
+                                    href="https://www.termsfeed.com/terms-conditions-generator /"
+                                    target="_blank"> shartlar va shartlar
+              Generator < /a>.< / p>
+        < / li>
+        <li>
+          <p>< strong>uchinchi tomon ijtimoiy Media xizmati < / strong> har qanday xizmat yoki tarkibni (shu jumladan
+            ma'lumotlarni) anglatadi,
+            axborot, mahsulotlar yoki xizmatlar) ko'rsatiladi mumkin uchinchi tomon tomonidan taqdim, kiritilgan yoki
+            amalga
+            xizmat tomonidan mavjud.< / p>
+        < / li>
+        <li>
+          <p>< strong>veb-sayt < / strong> < a href=" https://hant-travel.vercel.ilova/"
+                                               rel="tashqi noopener"
+                                               maqsad="_blank">https://hant-travel.vercel.app / < / a>
+          < / p>
+        < / li>
+        <li>
+          <p><strong>siz < /strong> xizmatga, kompaniyaga yoki boshqa qonuniy shaxsga kirish yoki undan foydalanishni
+            anglatadi
+            bunday shaxs nomidan xizmatga kiradigan yoki undan foydalanadigan shaxs.< / p>
+        < / li>
+      < / ul>
+      < h2>tasdiqlash< / h2>
+      < p>bu xizmatdan foydalanishni tartibga soluvchi shartlar va shartnoma
+        Siz va kompaniya. Ushbu shartlar va shartlar barcha foydalanuvchilarning huquq va majburiyatlarini belgilaydi
+        xizmatdan foydalanish.< / p>
+      <p>Sizning xizmatga kirishingiz va undan foydalanishingiz ushbu shartlarni qabul qilishingiz va ularga rioya
+        qilishingiz bilan bog'liq
+        Shartlar. Bu qoida va shartlar kirish yoki foydalanish, barcha mehmonlarga, foydalanuvchilar va boshqalar uchun
+        amal qiladi
+        Xizmat.< / p>
+      < p>xizmatga kirish yoki undan foydalanish orqali siz ushbu shartlar va shartlarga rioya qilishga rozilik
+        bildirasiz. Agar siz biron bir narsaga rozi bo'lmasangiz
+        ushbu shartlar va shartlarning bir qismi keyin siz xizmatga kira olmaysiz.< / p>
+      <p> siz 18 yoshdan oshganingizni bildirasiz. Kompaniya 18 yoshgacha bo'lganlarga foydalanishga ruxsat bermaydi
+        Xizmat.< / p>
+      < p>Sizning xizmatga kirishingiz va undan foydalanishingiz maxfiylikni qabul qilishingiz va unga rioya qilishingiz
+        bilan ham bog'liq
+        Kompaniya siyosati. Bizning Maxfiylik siyosati yig'ish bizning siyosat va tartib ta'riflaydi, foydalanish va
+        Agar ariza yoki veb-sayt foydalanish va haqida hikoya qachon shaxsiy ma'lumotlarni oshkor
+        maxfiylik huquqlari va qonun sizni qanday himoya qiladi. Iltimos, ishlatishdan oldin Maxfiylik siyosatimizni
+        diqqat bilan o'qing
+        Xizmat.< / p>
+      < h2>boshqa veb-saytlarga havolalar</h2>
+      < p>bizning xizmatimizda uchinchi tomon veb-saytlari yoki xizmatlariga tegishli bo'lmagan yoki boshqarilmaydigan
+        havolalar bo'lishi mumkin
+        Kompaniya.< / p>
+      < p>kompaniya tarkibni, maxfiylik siyosatini yoki amaliyotini nazorat qila olmaydi va javobgarlikni o'z zimmasiga
+        olmaydi
+        har qanday uchinchi tomon veb-saytlari yoki xizmatlari. Siz bundan keyin kompaniya bo'lmasligini tan olasiz va
+        rozilik bildirasiz
+        javobgar yoki javobgar, to'g'ridan-to'g'ri yoki bilvosita, etkazilgan yoki etkazilgan yoki taxmin qilingan har
+        qanday zarar yoki yo'qotish uchun
+        mavjud bo'lgan har qanday tarkib, tovarlar yoki xizmatlardan foydalanish yoki ularga ishonish bilan bog'liqlik
+        veb-saytlar yoki xizmatlar.< / p>
+      <p>biz sizga har qanday uchinchi tomon veb-saytlarining shartlari va maxfiylik siyosatini o'qib chiqishingizni
+        maslahat beramiz
+        agar tashrif xizmatlar.< / p>
+      < h2>tugatish < /h2>
+      <p>biz biron-bir sababga ko'ra oldindan ogohlantirmasdan yoki javobgarliksiz kirishni darhol to'xtatishimiz yoki
+        to'xtatishimiz mumkin
+        qat'i nazar,, cheklamagan holda, shu jumladan, siz bu shartlarni buzgan bo'lsa.< / p>
+      < p>xizmatdan foydalanish huquqingiz darhol to'xtatiladi.< / p>
+      < h2>javobgarlikni cheklash < /h2>
+      <p>har qanday zararga qaramay, kompaniya va uning yetkazib beruvchilarining barcha javobgarligi
+        ushbu shartlarning har qanday qoidalariga binoan va yuqorida aytilganlarning barchasi uchun sizning eksklyuziv
+        vositangiz quyidagilar bilan cheklanadi
+        xizmat orqali siz to'lagan summa yoki agar siz hech narsa sotib olmagan bo'lsangiz, 100 AQSh dollari
+        Xizmat.< / p>
+      <p>amaldagi qonunchilikda ruxsat etilgan maksimal darajada, hech qanday holatda kompaniya yoki uning etkazib
+        beruvchilari javobgar bo'lmaydi
+        har qanday maxsus, tasodifiy, bilvosita yoki bilvosita zarar (shu jumladan, lekin ular bilan cheklanmagan holda)
+        zarar
+        foyda yo'qotish, ma'lumotlar yoki boshqa ma'lumotlarni yo'qotish, biznesni to'xtatish, shaxsiy shikastlanish,
+        yo'qotish uchun
+        xizmat, uchinchi tomon dasturiy ta'minotidan foydalanish yoki undan foydalana olmaslik bilan bog'liq yoki
+        biron-bir tarzda yuzaga keladigan maxfiylik
+        va / yoki uchinchi tomon apparat xizmati bilan ishlatiladi, yoki boshqa bu shartlarini har qanday ta'minlash
+        munosabati bilan),
+        hatto kompaniya yoki biron bir etkazib beruvchiga bunday zarar etkazish ehtimoli haqida maslahat berilgan bo'lsa
+        ham va hatto chora ko'rilgan bo'lsa ham
+        uning asosiy maqsadi bajarilmaydi.< / p>
+      < p>ayrim davlatlar nazarda tutilgan kafolatlarni istisno qilishga yoki tasodifiy yoki javobgarlikni cheklashga
+        yo'l qo'ymaydi
+        natijada etkazilgan zarar, ya'ni yuqoridagi cheklovlarning ba'zilari qo'llanilmasligi mumkin. Ushbu Shtatlarda
+        har biri
+        partiyaning javobgarligi qonun tomonidan ruxsat etilgan darajada cheklanadi.< / p>
+      <h2> &quote;sifatida &quote; va & quote;sifatida mavjud&quote; voz kechish< / h2>
+      <p>xizmat sizga taqdim etiladi & quote;bo'lgani kabi&quote; va & quote;sifatida mavjud&quote; va barcha xato va
+        nuqsonlari bilan
+        har qanday kafolatsiz. Amaldagi qonunchilikka binoan ruxsat etilgan maksimal darajada, kompaniya o'z-o'zidan
+        nomidan va uning filiallari nomidan va uning va ularning tegishli litsenziya va xizmat ko'rsatuvchi
+        provayderlar, aniq
+        barcha kafolatlar voz, express bo'lsin, shama, rasmiy yoki boshqa, xizmatiga nisbatan,
+        sotilish barcha nazarda tutilgan kafolatlar, shu jumladan,, muayyan maqsad uchun fitnes, sarlavha va
+        muomala, ishlash, foydalanish yoki savdo jarayonida yuzaga kelishi mumkin bo'lgan huquqbuzarlik va kafolatlar
+        amaliyot. Yuqorida aytilganlarni cheklamasdan, kompaniya hech qanday kafolat yoki majburiyat bermaydi va hech
+        qanday kafolat bermaydi
+        xizmat sizning talablaringizga javob beradigan, istalgan natijalarga erishadigan har qanday turdagi vakillik
+        mos yoki har qanday boshqa dasturiy ta'minot bilan ishlash, ilovalar, tizimlari yoki xizmatlar, uzluksiz
+        faoliyat,
+        har qanday ishlash yoki ishonchlilik standartlariga javob bering yoki xatosiz bo'ling yoki har qanday xato yoki
+        nuqson bo'lishi mumkin yoki bo'lishi mumkin
+        tuzatilgan.< / p>
+      <p>yuqorida aytilganlarni cheklamasdan, na kompaniya, na kompaniyaning biron bir provayderi hech qanday vakillik
+        qilmaydi
+        yoki har qanday kafolat, aniq yoki nazarda tutilgan: (i) xizmatning ishlashi yoki mavjudligi yoki
+        unga kiritilgan ma'lumotlar, tarkib va materiallar yoki mahsulotlar; (ii) xizmat uzluksiz bo'lishi yoki
+        xatosiz; (iii) orqali taqdim etilgan har qanday ma'lumot yoki tarkibning aniqligi, ishonchliligi yoki valyutasi
+        to'g'risida
+        xizmat; yoki (iv) xizmat, uning serverlari, tarkibi yoki elektron pochta xabarlari nomidan yoki uning nomidan
+        yuborilgan
+        Kompaniyada viruslar, skriptlar, troyan otlari, qurtlar, zararli dasturlar, vaqt bombalari yoki boshqa zararli
+        komponentlar mavjud emas.< / p>
+      < p>ayrim yurisdiktsiyalar muayyan turdagi kafolatlar yoki cheklovlarni istisno qilishga ruxsat bermaydi
+        iste'molchining qonuniy huquqlari, shuning uchun yuqoridagi istisnolar va cheklovlarning bir qismi yoki barchasi
+        sizga tegishli bo'lmasligi mumkin. Lekin
+        bunday holda, ushbu bo'limda keltirilgan istisnolar va cheklovlar eng katta darajada qo'llaniladi
+        amaldagi qonun ostida majburiy.< / p>
+      < h2>boshqaruv Qonuni < /h2>
+      < p>mamlakat qonunlari, uning qonun qoidalariga zidligini hisobga olmaganda, ushbu shartlarni va sizning
+        Xizmat. Ilovadan foydalanishingiz boshqa mahalliy, shtat, milliy yoki xalqaro miqyosda ham bo'lishi mumkin
+        qonunlar.< / p>
+      < h2>nizolarni hal qilish< / h2>
+      <p>Agar sizda xizmat haqida biron bir tashvish yoki nizo bo'lsa, avval nizoni norasmiy ravishda hal qilishga
+        rozilik bildirasiz
+        kompaniyaga murojaat qilish orqali.< / p>
+      Evropa Ittifoqi (EI) foydalanuvchilari uchun <h2></h2>
+      <p>Agar siz Evropa Ittifoqining iste'molchisi bo'lsangiz, mamlakat qonunining har qanday majburiy qoidalaridan
+        foydalanasiz
+        unda siz rezidentsiz.< / p>
+      < h2>Amerika Qo'shma Shtatlari huquqiy muvofiqligi < /h2>
+      < p>siz AQShga bo'ysunadigan mamlakatda emasligingizni bildirasiz va kafolat berasiz
+        hukumat embargo, yoki Amerika qo'shma Shtatlari hukumati tomonidan tayinlangan qilingan &quote;terrorist
+        qo'llab-quvvatlash&quote; mamlakat, va (ii) siz taqiqlangan yoki har qanday Amerika qo'shma Shtatlari hukumati
+        ro'yxatida emas
+        cheklangan tomonlar.< / p>
+      < h2>Severability va voz kechish< / h2>
+      < h3>Severability< / h3>
+      <p>agar ushbu shartlarning har qanday qoidasi bajarilmaydigan yoki yaroqsiz deb topilsa, bunday qoida
+        o'zgartiriladi va
+        amaldagi qonunchilikka binoan bunday ta'minotning maqsadlarini iloji boricha ko'proq bajarish uchun talqin
+        qilingan
+        va qolgan qoidalar to'liq kuch va ta'sirda davom etadi.< / p>
+      < h3>voz kechish< / h3>
+      < p>bu erda nazarda tutilgan hollar bundan mustasno, huquqdan foydalanmaslik yoki majburiyatni bajarishni talab
+        qilmaslik
+        ushbu shartlar partiyaning bunday huquqdan foydalanish qobiliyatiga ta'sir qilmasligi yoki istalgan vaqtda
+        bunday ishlashni talab qilmasligi kerak
+        shundan so'ng ham bir buzilishi talab har qanday keyingi buzilishi bir istisno tashkil etadi.< / p>
+      < h2>tarjima talqini< / h2>
+      <p> ushbu shartlar, Agar biz ularni sizga xizmatimizda taqdim etgan bo'lsak, tarjima qilingan bo'lishi mumkin.
+        Siz nizo yuzaga kelganda asl inglizcha matn ustun bo'lishiga rozilik bildirasiz.< / p>
+      < h2>ushbu shartlar va shartlarga o'zgartirishlar< / h2>
+      < p> biz o'z xohishimiz bilan ushbu shartlarni istalgan vaqtda o'zgartirish yoki almashtirish huquqini saqlab
+        qolamiz. Agar reviziya bo'lsa
+        materiallar biz har qanday yangi shartlarni qabul qilishdan oldin kamida 30 kun oldin xabar berish uchun oqilona
+        harakat qilamiz
+        ta'siri. Moddiy o'zgarishni tashkil etadigan narsa bizning ixtiyorimiz bilan aniqlanadi.< / p>
+      < p>ushbu tahrirlar kuchga kirgandan keyin bizning xizmatimizga kirish yoki undan foydalanishni davom ettirish
+        orqali siz
+        qayta ko'rib shartlari. Agar siz yangi shartlarga to'liq yoki qisman rozi bo'lmasangiz, iltimos, veb-saytdan
+        foydalanishni to'xtating
+        Xizmat.< / p>
+      < h2>biz bilan bog'lanish< / h2>
+      < p>ushbu shartlar haqida savollaringiz bo'lsa, biz bilan bog'lanishingiz mumkin:< / p>
+      < ul>
+        <li>
+          < p>elektron pochta orqali: info@hant-travel.com< / p>
+        < / li>
+        <li>
+          < p>saytimizdagi ushbu sahifaga tashrif buyurib: < a href="https://hant-travel.vercel.ilova/"
+                                                               rel="tashqi noopener"
+                                                               target="_blank">https://hant-travel.vercel.app/</a></p>
+        < / li>
+        <li>
+          <p>telefon raqami orqali: +998978947323< / p>
+        < / li>
+      < / ul>
+    </>
+  );
+}
+export default uz;
