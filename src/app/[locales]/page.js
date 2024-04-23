7b042de9a8d7b894449eb3b247c7cd06d8cc91bd
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-04-23 18:35:38 +0500

    test

--- src/app/[locales]/page.js
+++ src/app/[locales]/page.js
@@ -1,15 +1,30 @@
-import Banner from "@/components/banner/Banner1";
-import BannerBottom from "@/components/banner/Banner1Bottom";
-import Destination from "@/components/destination/Destination1";
-import HomeFacilitySlide from "@/components/facilitySlide/Home1FacilitySlide";
-import HomeFacilities from "@/components/facilitySlide/Home1Fecilities2";
-import Footer from "@/components/footer/Footer";
-import Header from "@/components/header/Header";
-import HomeTourPackage from "@/components/tourPackage/Home1TourPackage";
-import HomeVise from "@/components/visaComponents/Home1Vise";
-import HomeWhyChoose from "@/components/whyChoose/Home1WhyChoose";
-import VideoSection from "@/components/videoSection/Home2VideoSection";
+import dynamic from "next/dynamic";
+// import Header from "@/components/header/Header";
+// import Banner from "@/components/banner/Banner1";
+// import BannerBottom from "@/components/banner/Banner1Bottom";
+// import Destination from "@/components/destination/Destination1";
+// import HomeFacilitySlide from "@/components/facilitySlide/Home1FacilitySlide";
+// import HomeFacilities from "@/components/facilitySlide/Home1Fecilities2";
+// import Footer from "@/components/footer/Footer";
+// import HomeTourPackage from "@/components/tourPackage/Home1TourPackage";
+// import HomeVise from "@/components/visaComponents/Home1Vise";
+// import HomeWhyChoose from "@/components/whyChoose/Home1WhyChoose";
+// import VideoSection from "@/components/videoSection/Home2VideoSection";
+
+const Header = dynamic(() => import("@/components/header/Header"));
+const Banner = dynamic(() => import("@/components/banner/Banner1"))
+const BannerBottom = dynamic(() => import("@/components/banner/Banner1Bottom"))
+const Destination = dynamic(() => import("@/components/destination/Destination1"))
+const HomeFacilitySlide = dynamic(() => import("@/components/facilitySlide/Home1FacilitySlide"))
+const HomeFacilities = dynamic(() => import("@/components/facilitySlide/Home1Fecilities2"))
+const HomeTourPackage = dynamic(() => import("@/components/tourPackage/Home1TourPackage"))
+const HomeVise = dynamic(() => import("@/components/visaComponents/Home1Vise"))
+const Footer = dynamic(() => import("@/components/footer/Footer"))
+const HomeWhyChoose = dynamic(() => import("@/components/whyChoose/Home1WhyChoose"))
+const VideoSection = dynamic(() => import("@/components/videoSection/Home2VideoSection"))
+
 import {useTranslations} from 'next-intl';
+
 export const metadata = {
   title: "HANT TRAVEL",
   description: "Hant travel tour agency official webpage",
@@ -168,7 +183,7 @@ export default function Home() {
       text: vi("tax")
     },
     {
-      text:vi("eVisa")
+      text: vi("eVisa")
     }
 
   ]
